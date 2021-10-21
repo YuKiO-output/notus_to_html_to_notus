ab909ec508d0361de99562eb5f30043b877fa48f
YuKiO-output <76410303+YuKiO-output@users.noreply.github.com>
2021-10-21 10:47:32 +0900

    update  Attributes Model to add italic Property

--- lib/src/models/attributes.dart
+++ lib/src/models/attributes.dart
@@ -2,17 +2,20 @@ class Attributes {
   int? heading = 0;
   String? block = '';
   bool? b = false;
+  bool? i = false;
 
   /* NotusAttribute.italic
   NotusAttribute.link*/
 
-  Attributes({this.heading, this.block, this.b});
+  Attributes({this.heading, this.block, this.b, this.i});
 
   factory Attributes.fromJson(dynamic json) {
+    print("attributesの中身$json");
     return Attributes(
       heading: json['heading'],
       block: json['block'],
       b: json['b'],
+      i: json["i"],
     );
   }
 
@@ -21,6 +24,7 @@ class Attributes {
       'heading': heading,
       'block': block,
       'b': b,
+      'i': i,
     };
   }
 }
